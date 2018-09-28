1218661ed42fc082532dc1766657a94f59ab0d41
luba-kn <luba_kn@mail.ru>
2018-09-28 13:14:21 +0300

    второе дз

---
+++ blog/config/routes.rb
@@ -0,0 +1,5 @@
+Rails.application.routes.draw do
+  get 'about', to: 'home#about'
+  root 'home#index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
