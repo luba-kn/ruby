9a3e8cedf33f72f63bb766f5bb5c19383f48fe72
luba-kn <luba_kn@mail.ru>
2018-10-08 08:53:51 +0300

    добавила devise, немного изменила внешний вид формы (стили в application.scss)

--- blog/db/schema.rb
+++ blog/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_02_091900) do
+ActiveRecord::Schema.define(version: 2018_10_07_150240) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,6 +57,10 @@ ActiveRecord::Schema.define(version: 2018_10_02_091900) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "comments_count"
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["name"], name: "index_users_on_name", unique: true
   end
